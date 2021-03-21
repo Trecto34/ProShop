6b0b7c031355d82fe1b5027d043ece1208c7ff8d
Adan de Oliveira <anonymu616@gmail.com>
2021-03-21 17:25:54 -0300

    Connect backend to frontend
    Signed-off-by: Adan de Oliveira <anonymu616@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,8 @@
+
 from pathlib import Path
 import os
+
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -29,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'base',
 ]
@@ -36,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
